6c130287faefa070ec92ed79fde41a7e0b8abe46
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-08-02 15:26:42 -0300

    Customers, products and orders route validations

--- src/modules/orders/infra/http/routes/orders.routes.ts
+++ src/modules/orders/infra/http/routes/orders.routes.ts
@@ -1,11 +1,37 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import OrdersController from '../controller/OrdersController'
 
 const ordersRouter = Router()
 const ordersController = new OrdersController()
 
-ordersRouter.post('/', ordersController.create)
-ordersRouter.get('/:id', ordersController.show)
+ordersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      customer_id: Joi.string().uuid().required(),
+      products: Joi.array()
+        .items(
+          Joi.object().keys({
+            id: Joi.string().required(),
+            quantity: Joi.number().integer().required()
+          })
+        )
+        .required()
+    }
+  }),
+  ordersController.create
+)
+
+ordersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    }
+  }),
+  ordersController.show
+)
 
 export default ordersRouter
--- src/modules/products/infra/http/routes/products.routes.ts
+++ src/modules/products/infra/http/routes/products.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import ProductsController from '../controller/ProductsController'
 
 const productsRouter = Router()
 const productsController = new ProductsController()
 
-productsRouter.post('/', productsController.create)
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().required(),
+      quantity: Joi.number().integer().required()
+    }
+  }),
+  productsController.create
+)
 
 export default productsRouter
