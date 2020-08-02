5cbbef4f7b489c16574fefa3fc99641ee4480046
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-08-02 12:55:33 -0300

    Orders controller, service and repository

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,23 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id)
+    if (!customer) throw new AppError('Customer not found')
+
+    const order = this.ordersRepository.create({ customer, products })
+
+    return order
   }
 }
 
